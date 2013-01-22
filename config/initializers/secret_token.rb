207ee9f8b8551cfcf3f8c6ca20ca8bbeac1a9e27
Luke Price <luke@coastlandmobile.com>
2013-01-22 12:49:10 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lionheart::Application.config.secret_token = 'd87f661ad9898f6c0b6b6d4cdfe80b98d579e0862cdd45838c6611362300f856d74211c3c8b43675b6a2194affe65a85bed8e4a7f1248760718a4963397bb27d'
