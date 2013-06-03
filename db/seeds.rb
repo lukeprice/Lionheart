6bc2d8386ee8e4d1903d73338e5e5aaae3a5f3c0
Luke Price <luke@coastlandmobile.com>
2013-06-03 10:32:56 -0400

    Change email addresses to production values.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Admin.create!(:email => 'luke@bpea.com', :password => 'test1234')
+Admin.create!(:email => 'alex.austman@gmail.com', :password => 'T3mpP@ss1')
